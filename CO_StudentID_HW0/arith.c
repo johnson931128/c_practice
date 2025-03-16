492e6c202072cf36b420d6bfd09d9cf0f97a0201
johnson931128 <e94126054@gs.ncku.edu.tw>
2025-03-16 06:36:24 +0000

    Move arith.c to CO_StudentID_HW0 folder

--- CO_StudentID_HW0/arith.c
+++ CO_StudentID_HW0/arith.c
@@ -7,8 +7,7 @@ int main()
     FILE *input = fopen("../testcases/input/1.txt", "r");
     fscanf(input, "%d %d", &a, &b);
     fclose(input);
-    /* a = a - b */
-    asm volatile(/* Your Code */);
+    asm volatile("sub %[A], %[A], %[B]\n\t" : [A] "+r"(a) : [B] "r"(b));
     printf("%d\n", a);
     return 0;
 }
