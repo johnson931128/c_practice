2a7137e1bad1d519df440f3a83fe59864b8cd7c5
johnson931128 <e94126054@gs.ncku.edu.tw>
2025-04-25 06:02:40 +0000

    New

--- CO_StudentID_HW2/CO_StudentID_HW2/macro_define.h
+++ CO_StudentID_HW2/CO_StudentID_HW2/macro_define.h
@@ -9,18 +9,26 @@
                       		(long long)fsub_cnt * fsub_CPI + \
                       		(long long)fmul_cnt * fmul_CPI + \
                       		(long long)fdiv_cnt * fdiv_CPI + \
-                      		(long long)others_cnt * others_CPI )
+                      		(long long)others_cnt * others_CPI );
 
 #define macro_fft_cpu_time \
-	fft_cpu_time = ( (double)fft_cycle_count * cycle_time / 1000.0)
+	fft_cpu_time = ( (double)fft_cycle_count * cycle_time );
 
 #define macro_calc_fft_ratio \
-	fft_ratio = ( (fft_cycle_count > 0) ? \
-			( (double)fft_cycle_count - \
-				( (long long)lw_cnt * lw_CPI + \
-				(long long)sw_cnt * sw_CPI + \
-				/ fft_cycle_count) \
-				:0.0)
+    fft_ratio = ( (fft_cycle_count > 0) ? \
+                  ( \
+                    ( (double)(fft_cycle_count - \
+                                ( \
+                                  (long long)lw_cnt * lw_CPI + \
+                                  (long long)sw_cnt * sw_CPI \
+                                ) \
+                              ) \
+                    ) / fft_cycle_count \
+                  ) \
+                  : \
+                  0.0 \
+                );
+
 
 #define macro_arraymul_vector_cycle_count arraymul_vector_cycle_count = 0;
 
